85e019d822a8b5766ff0d8cd075f03581117c987
Olawale <walexawit@yahoo.com>
2017-08-17 16:26:58 -0400

    final project again

--- CSSI/MADLIBS/ABBY/CSSI-master/Newapp/ABBY/main.py
+++ CSSI/MADLIBS/ABBY/CSSI-master/Newapp/ABBY/main.py
@@ -3,8 +3,9 @@ import webapp2
 import json
 import logging
 
-
 import urllib
+import urllib2
+
 import jinja2
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
@@ -46,10 +47,53 @@ class latlongHandler(webapp2.RequestHandler):
 
 
         lat_template = env.get_template('mapdir.html')
-        template_dicts = {'o_lat': o_template_dicts['lat'], 'o_long' : o_template_dicts['lng'], 'd_lat':d_template_dicts['lat'], 'd_long' : d_template_dicts['lng'], 'uber_quote': 7.99}
+
+        base_url = "https://api.uber.com/v1.2/estimates/price?"
+        url_params = {'start_latitude': o_template_dicts['lat'], 'start_longitude': o_template_dicts['lng'], 'end_latitude':d_template_dicts['lat'] ,'end_longitude': d_template_dicts['lng']}
+
+        r = urllib2.Request(base_url + urllib.urlencode(url_params))
+        r.add_header("Authorization", "Token mdLSqYtUTVL6S346q4HbdGChonPo7GlItyB4feBI")
+        r.add_header("Content-Type", "application/json")
+        r.add_header("Accept-Language", "en_EN")
+
+        resp = urllib2.urlopen(r)
+        resp_data = resp.read()
+        resp_dict = json.loads(resp_data)
+        prices = resp_dict['prices']
+        string_prices=''
+        for item in prices:
+            string_prices= string_prices + item['localized_display_name'] + " " + str(item['low_estimate']) + "-" + str(item['high_estimate']) + " "
+
+        template_dicts = {'o_lat': o_template_dicts['lat'], 'o_long' : o_template_dicts['lng'], 'd_lat':d_template_dicts['lat'], 'd_long' : d_template_dicts['lng'], 'uber_quote': string_prices}
         self.response.write(lat_template.render(template_dicts))
+
         #request_str='/mapdir?lat=%s&lng=%s' % (d_template_dicts['lat'],d_template_dicts['lng'])
         #self.redirect(request_str)
+
+class UberHandler(webapp2.RequestHandler):
+    def get(self):
+        #main_template = env.get_template('address.html')
+        #self.response.out.write(main_template.render())
+
+        #query = self.request.get('q')
+        #logging.info("QUERY:" + query)
+        base_url = "https://api.uber.com/v1.2/estimates/price?"
+        url_params = {'start_latitude': "40.6631115", 'start_longitude': "-73.9565514", 'end_latitude':"40.6653747" ,'end_longitude': "-73.9654139"}
+
+        r = urllib2.Request(base_url + urllib.urlencode(url_params))
+        r.add_header("Authorization", "Token mdLSqYtUTVL6S346q4HbdGChonPo7GlItyB4feBI")
+        r.add_header("Content-Type", "application/json")
+        r.add_header("Accept-Language", "en_EN")
+
+        resp = urllib2.urlopen(r)
+        resp_data = resp.read()
+        resp_dict = json.loads(resp_data)
+        prices = resp_dict['prices']
+        string_prices=''
+        for item in prices:
+            string_prices=string_prices + item['localized_display_name'] + " " + str(item['low_estimate']) + " " + str(item['high_estimate']) + " "
+        self.response.write(string_prices)
+
 
 class MapHandler(webapp2.RequestHandler):
     def get (self):
@@ -81,5 +125,6 @@ app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/gip', GipHandler),
     ('/lat', latlongHandler),
-    ('/map', MapHandler)
+    ('/map', MapHandler),
+    ('/uber', UberHandler),
 ], debug=True)
